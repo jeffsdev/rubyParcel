1b85cdcf8e7bedd6bf47bbc8c2e86db61bc4ea99
Jeff Seymour and Kyle Langley <seym1585@gmail.com>
2016-01-11 10:47:47 -0800

    add spec to check parcel volume

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,5 +1,11 @@
 class Parcel
-  define_method(:initialize)  do
-    
+  define_method(:initialize)  do |h, w, d, weight|
+    @height = h.to_i
+    @width = w.to_i
+    @depth = d.to_i
+    @weight = weight.to_i
+  end
+  define_method(:volume) do
+    return @height * @width * @depth
   end
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -2,5 +2,7 @@ require('rspec')
 require('parcel')
 
 describe("Parcel#parcel") do
-  
+  it("will find the volume of Parcel") do
+    expect(Parcel.new(2,4,5,10).volume).to(eq(40))
+  end
 end
