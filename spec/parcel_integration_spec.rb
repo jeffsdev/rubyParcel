f332f3c97371caac5f2634ff3ed26c19155abaa9
Jeff Seymour and Kyle Langley <altck8@gmail.com>
2016-01-11 11:36:02 -0800

    add integration spec for volume

---
+++ spec/parcel_integration_spec.rb
@@ -0,0 +1,19 @@
+require('capybara/rspec')
+require('./app')
+
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe("Parcel#parcel", {:type => :feature}) do
+  it("will take user input and calculate volume") do
+    visit('/')
+    fill_in('h', :with => "5")
+    fill_in('w', :with => "5")
+    fill_in('d', :with => "5")
+    fill_in('weight', :with => "4")
+    click_button('send')
+    expect(page).to have_content('125')
+  end
+
+
+end
